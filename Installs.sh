46f54513f4bbf1a33fd2b576a71126937b61327b
Inderpal Singh <rajan_yourfriend@yahoo.com>
2013-05-16 12:46:10 +0530

    Create Installs.sh
    Pre installs for running code (r-pi)

---
+++ Installs.sh
@@ -0,0 +1,26 @@
+#Dependencies
+apt-get update
+apt-get install python
+apt-get install git
+apt-get install python-pip
+apt-get install python-setuptools
+pip install pyserial
+pip install -U minimalmodbus
+pip install -U pymodbus
+apt-get install libzmq-dev
+apt-get install python-dev
+apt-get install libevent-dev
+pip install pyzmq gevent gevent_zeromq paste 
+pip install pytz
+pip install gevent-websocket
+pip install requests
+apt-get install nano
+apt-get install cython
+
+#For using that pc utilization info for logs
+#easy_install pcutil
+
+# For adafruit Web-ide un-comment following two lines
+
+#curl https://raw.github.com/adafruit/Adafruit-WebIDE/alpha/scripts/install.sh | sudo sh
+#sudo service adafruit-webide.sh start
