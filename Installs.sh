f4e0947a8a90c560b8eb3d794c459e50a7c3a2ea
Inderpal Singh <Inderpals@iiitd.ac.in>
2013-06-19 15:19:46 +0530

    moving cython up in list

--- Installs.sh
+++ Installs.sh
@@ -7,6 +7,7 @@ apt-get install python-setuptools
 pip install pyserial
 pip install -U minimalmodbus
 pip install -U pymodbus
+apt-get install cython
 apt-get install libzmq-dev
 apt-get install python-dev
 apt-get install libevent-dev
@@ -15,7 +16,6 @@ pip install pytz
 pip install gevent-websocket
 pip install requests
 apt-get install nano
-apt-get install cython
 
 #For using that pc utilization info for logs
 #easy_install psutil
